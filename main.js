0e7d0d4e971c2b29102617552934937d00e82382
dlsso <supertonesfan@hotmail.com>
2014-06-27 11:51:28 -0600

    Made submit button update user profile

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 $(document).on('ready', function() {
   
-	$( "#profile-form" ).submit(function( event ) {
+	$( "#profile-form" ).submit(function( event ) {		// On click of submit button...
 	  event.preventDefault();
 
-	  	// newText = $(this).val()
-	  	// console.log(newText)
-	  	// console.log(newText)
-	  	// $(this).parent().html(newText)
+	  // Store field values as varibables
+	  var user = $('#profile-form input[name=user]').val()
+	  var books = $('#profile-form input[name=books]').val()
+	  var libs = $('#profile-form input[name=libs]').val()
+	  var bio = $('#profile-form input[name=bio]').val()
 
+	  // Update profile
+	  $('#user').empty().append("Name: "+ user)
+	  $('#books').empty().append("Favorite books: "+ books)
+	  $('#libs').empty().append("Favorite Javascript libraries: "+ libs)
+	  $('#bio').empty().append(bio)
 
 	});
 
